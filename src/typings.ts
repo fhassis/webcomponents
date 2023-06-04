ffa938150cf9f35bdfdcea35734fcbf02db9f172
fhassis <fhassis2@gmail.com>
2023-06-04 20:09:51 -0300

    formatted with prettier.

--- src/typings.ts
+++ src/typings.ts
@@ -3,25 +3,25 @@ import type { OhlcData } from "lightweight-charts";
 export type PositionType = "BUY" | "SELL";
 
 export type OrderType =
-  | "BUY_MARKET"
-  | "SELL_MARKET"
-  | "BUY_LIMIT"
-  | "SELL_LIMIT";
+	| "BUY_MARKET"
+	| "SELL_MARKET"
+	| "BUY_LIMIT"
+	| "SELL_LIMIT";
 
 export interface ICandle extends OhlcData {}
 
 interface BaseOrder {
-  id: string;
-  price: number;
-  amount: number;
-  sl: number;
-  tp: number;
+	id: string;
+	price: number;
+	amount: number;
+	sl: number;
+	tp: number;
 }
 
 export interface IOrder extends BaseOrder {
-  type: OrderType;
+	type: OrderType;
 }
 
 export interface IPosition extends BaseOrder {
-  type: PositionType;
+	type: PositionType;
 }
